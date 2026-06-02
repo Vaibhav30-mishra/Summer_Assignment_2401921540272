46132b19f7231640a612a5b0a703a9e8b2cd4628
Vaibhav30-mishra <vaibhavmishra7908@gmail.com>
2026-06-02 21:20:46 +0530

    Create MaximumAverageSubarray.cpp

---
+++ Week_1/Day_2/MaximumAverageSubarray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int sum=0;
+        for(int i=0;i<k;i++){
+            sum+=nums[i];
+        }
+        int maxs=sum;
+        for(int i=k;i<nums.size();i++){
+            sum=sum-nums[i-k]+nums[i];
+            maxs=max(maxs,sum);
+        }
+        return double(maxs)/k;
+
+    }
+};
