59fddc3c60b6f06ade92075d96788c8965350a8a
Vaibhav30-mishra <vaibhavmishra7908@gmail.com>
2026-06-15 22:27:39 +0530

    Create MiddleOfLinkedList.cpp

---
+++ Week_3/Day_1/MiddleOfLinkedList.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* first = head;
+        ListNode* second = head;
+
+        while (second != NULL && second->next != NULL) {
+            first = first->next;
+            second = second->next->next;
+        }
+
+        return first;
+    }
+};
