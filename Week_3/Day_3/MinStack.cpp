ea4d6f77e8845ee25434dc23269016b14dc07957
Vaibhav30-mishra <vaibhavmishra7908@gmail.com>
2026-06-17 21:15:28 +0530

    Create MinStack.cpp

---
+++ Week_3/Day_3/MinStack.cpp
@@ -0,0 +1,30 @@
+class MinStack {
+    stack<int> data;
+    stack<int> mins;
+
+public:
+    MinStack() {
+    }
+
+    void push(int val) {
+        data.push(val);
+
+        if (mins.empty() || val <= mins.top())
+            mins.push(val);
+    }
+
+    void pop() {
+        if (data.top() == mins.top())
+            mins.pop();
+
+        data.pop();
+    }
+
+    int top() {
+        return data.top();
+    }
+
+    int getMin() {
+        return mins.top();
+    }
+};
