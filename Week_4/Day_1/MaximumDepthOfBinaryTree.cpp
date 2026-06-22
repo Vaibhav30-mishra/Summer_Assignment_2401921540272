c217f7a8815bcb8b96f587614c0dbf5dda025606
Vaibhav30-mishra <vaibhavmishra7908@gmail.com>
2026-06-22 21:57:14 +0530

    Create MaximumDepthOfBinaryTree.cpp

---
+++ Week_4/Day_1/MaximumDepthOfBinaryTree.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (!root) return 0;
+
+        int left = maxDepth(root->left);
+        int right = maxDepth(root->right);
+
+        return 1 + (left > right ? left : right);
+    }
+};
